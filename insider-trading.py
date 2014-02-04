c8d534fd02cfa0bee4d9c48ded211ebd90d67bc7
Akshay <aktwo@princeton.edu>
2014-02-03 21:08:10 -0500

    Added ability to read text file of tickers

--- insider-trading.py
+++ insider-trading.py
@@ -4,59 +4,77 @@ import datetime
 import re
 from bs4 import BeautifulSoup
 
+# First argument is the historical window that we want to search
 numDays = int(argv[1])
-tickers = argv[2:]
 
+# Text file containing list of tickers
+tickerFile = argv[2]
+
+# Helper function to get list of tickers from file
+def getTickers(tickerFile):
+  return [line.rstrip('\n') for line in open(tickerFile)]
+
+# Helper function to parse a row of data in the insider trade table
 def parseRow(row, data):
-
   entry = row.find_all('td')
   dateString = entry[0].string
   date = datetime.datetime.strptime(dateString, "%b %d, %Y").date()
   now = datetime.datetime.now().date()
 
+  # If trade is outside historical window, discard it
   if ((now-date).days > numDays):
     return
+
+  # If trade is inside historical window, use it
   else:
-    # Name is entry[1].a.string
-    # Executive title is entry[1].span.string
-    tradeSize = int(re.sub(',', '', entry[2].string.encode('ascii', 'ignore')))
-    tradeType = getTradeType(entry[4].string.encode('ascii', 'ignore'))
+    try:
+      tradeSize = int(re.sub(',', '', entry[2].string))
+    except:
+      tradeSize = 0
+    tradeType = getTradeType(entry[4].string)
     try:
       data[tradeType] = data[tradeType] + tradeSize
     except KeyError:
       data[tradeType] = tradeSize
 
+# Helper function to parse the trade type
 def getTradeType(string):
   pattern = re.compile("(?P<tradeType>.*) at (.*) per share")
-  tradeType = pattern.search(string).groupdict()["tradeType"]
-  #This will be either sale, purchase or other things
-  if(tradeType):
-    return tradeType
-  else:
-    return "Other"
+  try:
+    tradeType = pattern.search(string).groupdict()["tradeType"]
+  except AttributeError:
+    tradeType = "Other"
+  return tradeType
 
 # Main loop
+errorTickers = ""
 insiderActivity = {}
+tickers = getTickers(tickerFile)
 for ticker in tickers:
-  url = "http://finance.yahoo.com/q/it?s=" + ticker + "+Insider+Transactions"
 
+  #Open URL and extract insider trading table
+  url = "http://finance.yahoo.com/q/it?s=" + ticker + "+Insider+Transactions"
   html = urllib2.urlopen(url).read()
   soup = BeautifulSoup(html, 'lxml')
-  table = soup.find_all('table')[14]
-  rows = table.find_all('tr')
+  try:
+    table = soup.find_all('table')[14]
+    rows = table.find_all('tr')
+    # For each row, populate the number of purchases for that ticker
+    trades = {}
+    for row in rows[1:]:
+      parseRow(row, trades)
+    # Get total number of purchases
+    try:
+      insiderActivity[ticker] = trades["Purchase"]
+    except:
+      insiderActivity[ticker] = 0
+  except:
+    errorTickers = ticker + ", " + errorTickers
+    insiderActivity[ticker] = 0
 
-  trades = {}
+finalResult = sorted(insiderActivity.items(), key=lambda x: x[1], reverse=True)
+for item in finalResult:
+  print item[0].upper() + " : " + str(item[1])
 
-  # number of transactions (reduce to increase speed)
-  for row in rows[1:]:
-    parseRow(row, trades)
-
-  print ticker.upper()
-  for trade in trades:
-    print trade + " : " + str(trades[trade])
-  print "-" * 40
-
-  insiderActivity[ticker] = trades
-
-#print stocks
-# http://ichart.finance.yahoo.com/table.csv?s=GE&a=00&b=2&c=1962&d=01&e=2&f=2014&g=d&ignore=.csv
+print "The following tickers could not be loaded:"
+print errorTickers
