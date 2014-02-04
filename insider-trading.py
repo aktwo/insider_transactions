83121f2d628438c1ce9d81e9900918a353a61045
Akshay <aktwo@princeton.edu>
2014-02-03 20:13:46 -0500

    Aggregate trades by transaction type

--- insider-trading.py
+++ insider-trading.py
@@ -4,14 +4,12 @@ import datetime
 import re
 from bs4 import BeautifulSoup
 
-stocks = {}
 numDays = int(argv[1])
 tickers = argv[2:]
 
 def parseRow(row, data):
-  transaction = {}
-  entry = row.find_all('td')
 
+  entry = row.find_all('td')
   dateString = entry[0].string
   date = datetime.datetime.strptime(dateString, "%b %d, %Y").date()
   now = datetime.datetime.now().date()
@@ -19,23 +17,16 @@ def parseRow(row, data):
   if ((now-date).days > numDays):
     return
   else:
-    transaction["Date"] = entry[0].string
-    transaction["Name"] = entry[1].a.string
-    transaction["Title"] = entry[1].span.string
-    transaction["Shares"] = entry[2].string
-    transaction["Direction"] = parseTrade(entry[4].string)
+    # Name is entry[1].a.string
+    # Executive title is entry[1].span.string
+    tradeSize = int(re.sub(',', '', entry[2].string.encode('ascii', 'ignore')))
+    tradeType = getTradeType(entry[4].string.encode('ascii', 'ignore'))
+    try:
+      data[tradeType] = data[tradeType] + tradeSize
+    except KeyError:
+      data[tradeType] = tradeSize
 
-    for key in transaction:
-      print key + " : " + transaction[key]
-
-    print "-" * 40
-
-    transaction["Date"] = 5
-    transaction["Position"] = row.td.next_sibling.span.string
-    data.append(transaction)
-
-# FIXME: make buy, sell or null
-def parseTrade(string):
+def getTradeType(string):
   pattern = re.compile("(?P<tradeType>.*) at (.*) per share")
   tradeType = pattern.search(string).groupdict()["tradeType"]
   #This will be either sale, purchase or other things
@@ -44,6 +35,8 @@ def parseTrade(string):
   else:
     return "Other"
 
+# Main loop
+insiderActivity = {}
 for ticker in tickers:
   url = "http://finance.yahoo.com/q/it?s=" + ticker + "+Insider+Transactions"
 
@@ -52,13 +45,18 @@ for ticker in tickers:
   table = soup.find_all('table')[14]
   rows = table.find_all('tr')
 
-  data = []
+  trades = {}
 
   # number of transactions (reduce to increase speed)
   for row in rows[1:]:
-    parseRow(row, data)
+    parseRow(row, trades)
 
-  stocks[ticker] = data
+  print ticker.upper()
+  for trade in trades:
+    print trade + " : " + str(trades[trade])
+  print "-" * 40
+
+  insiderActivity[ticker] = trades
 
 #print stocks
 # http://ichart.finance.yahoo.com/table.csv?s=GE&a=00&b=2&c=1962&d=01&e=2&f=2014&g=d&ignore=.csv
